a0900ad16e5cb69b539c907e832261310c159a10
juan-phzy <jp.hernandez.yz@gmail.com>
2023-12-27 20:19:32 -0500

    commit

--- components/shared/Topbar.tsx
+++ components/shared/Topbar.tsx
@@ -1,7 +1,13 @@
+"use client";
+
 import Link from "next/link";
 import Image from "next/image";
+import { useRouter } from "next/navigation";
+import { OrganizationSwitcher, SignOutButton, SignedIn } from "@clerk/nextjs";
+import { dark } from "@clerk/themes";
 
 function Topbar() {
+	const router = useRouter();
 	return (
 		<nav className="topbar">
 			<Link href="/" className="flex items-center gap-4">
@@ -10,6 +16,34 @@ function Topbar() {
 					Threads
 				</p>
 			</Link>
+
+			<div className="flex items-center gap-1">
+				<div className="block md:hidden">
+					<SignedIn>
+						<SignOutButton
+							signOutCallback={() => router.push("/sign-in")}
+						>
+							<div className="flex cursor-pointer">
+								<Image
+									src="/assets/logout.svg"
+									alt="logout"
+									width={24}
+									height={24}
+								/>
+							</div>
+						</SignOutButton>
+					</SignedIn>
+				</div>
+
+				<OrganizationSwitcher
+					appearance={{
+						baseTheme: dark,
+						elements: {
+							organizationSwitcherTrigger: "py-2 px-4",
+						},
+					}}
+				/>
+			</div>
 		</nav>
 	);
 }
